f955c046c30a75857b5dc7fde334816b6dfcc562
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2024-06-11 18:43:48 +0200

    Added Dynamic CSP policy and Dyanmic image hosts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,38 @@ import { NextResponse, type NextRequest } from "next/server"
 
 export function middleware(request: NextRequest)
 {
+    const optimizelyCmsUrl = process.env.OPTIMIZELY_CMS_URL ?? ''
+
+    // Add CSP
+    const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+    const cspHeader = `
+        default-src 'self';
+        script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${ optimizelyCmsUrl };
+        style-src 'self' 'nonce-${nonce}';
+        img-src 'self' ${ optimizelyCmsUrl } blob: data:;
+        font-src 'self';
+        object-src 'none';
+        base-uri 'self';
+        form-action 'self' ${ optimizelyCmsUrl };
+        frame-ancestors ${ optimizelyCmsUrl || 'none'};
+        upgrade-insecure-requests;
+    `
+    // Replace newline characters and spaces
+    const contentSecurityPolicyHeaderValue = cspHeader.replace(/\s{2,}/g, ' ').trim()
+
+    // Make the nonce & policy available to the request
+    const requestHeaders = new Headers(request.headers)
+    requestHeaders.set('x-nonce', nonce)
+    requestHeaders.set('Content-Security-Policy', contentSecurityPolicyHeaderValue)
+    const response = NextResponse.next({
+        request: {
+            headers: requestHeaders,
+        },
+    })
+
+    // Add the policy to the response
+    response.headers.set('Content-Security-Policy', contentSecurityPolicyHeaderValue)
+
     /*
     // We're in dev mode when the environment is not production or the host includes localhost
     const isDev = process.env.NODE_ENV != "production" || request.nextUrl.host.includes("localhost")
