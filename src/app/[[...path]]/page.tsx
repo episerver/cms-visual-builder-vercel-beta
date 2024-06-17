de7cdaf19b85a3f9df352cb1a5979cdfea02cebd
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2024-06-17 17:31:03 +0200

    Fixed Optimizely Graph Configuration

--- src/app/[[...path]]/page.tsx
+++ src/app/[[...path]]/page.tsx
@@ -1,10 +1,17 @@
-import { CmsPage } from '@remkoj/optimizely-cms-nextjs'
+import { CmsPage, getServerClient } from '@remkoj/optimizely-cms-nextjs'
 import createFactory from '@/components'
 import { getContentByPath } from '@/gql'
 
 const { CmsPage:OptimizelyPage, generateMetadata, generateStaticParams } = CmsPage.createPage(createFactory(), {
     //@ts-expect-error We have the actual types on this query, not on the generic one
-    getContentByPath
+    getContentByPath,
+    client: () => {
+        const client = getServerClient()
+        client.updateFlags({
+            queryCache: false // We're depending on @recursive & cursors, which don't work with the queryCache
+        })
+        return client
+    }
 })
 
 export const dynamic = "force-static"; // Make sure we cache pages
